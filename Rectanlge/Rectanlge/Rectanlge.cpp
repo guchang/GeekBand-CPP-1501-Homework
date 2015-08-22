bb58166c81616a8f5f73caabcb7a6f728fb0b6a5
pageye <xssptmxk@gmail.com>
2015-08-22 18:51:52 +0800

    第一次作业

---
+++ Rectanlge/Rectanlge/Rectanlge.cpp
@@ -0,0 +1,13 @@
+// Rectanlge.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include "Rectangle.h"
+using namespace std;
+int main()
+{
+	Rectangle rectangle1(3, 4, 0, 0);
+	Rectangle rectangle2(1, 1, 0, 0);
+    return 0;
+}
+
